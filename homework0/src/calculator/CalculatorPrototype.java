12b877e4388b7fea73fe45a2d96acdb45adfb282
Orlando Leombruni <o.leombruni@gmail.com>
2017-09-30 17:27:51 +0200

    comments removed

--- homework0/src/calculator/CalculatorPrototype.java
+++ homework0/src/calculator/CalculatorPrototype.java
@@ -2,13 +2,6 @@ package calculator;
 
 public class CalculatorPrototype implements CalculatorIF {
 
-    /**
-     *
-     * @param m the first addend
-     * @param n the second addend
-     * @return
-     */
-
     @Override
     public int sum(int m, int n) {
 

dce9706ce5b46d49e612b453d77b9e21a28e09da
Orlando Leombruni <o.leombruni@gmail.com>
2017-09-30 11:40:33 +0200

    first commit

---
+++ homework0/src/calculator/CalculatorPrototype.java
@@ -0,0 +1,103 @@
+package calculator;
+
+public class CalculatorPrototype implements CalculatorIF {
+
+    @Override
+    public int sum(int m, int n) {
+
+        /*
+         * Contract: sum(m, n) must perform n increments (+1)
+         * to the value of m, and return the result
+         */
+
+        int result = m;
+
+        if (n >= 0) {
+            while(n > 0) {
+                result += 1;
+                n -= 1;
+            }
+        } else {
+            /*
+                When n is negative, we treat the sum as a subtraction
+             */
+            result = subtract(m, -n);
+        }
+
+        return result;
+    }
+
+    @Override
+    public int subtract(int m, int n) {
+
+        /*
+         * Contract: subtract(m, n) must perform n decrements (-1)
+         * to the value of m, and return the result
+         */
+
+        int result = m;
+
+        if (n >= 0) {
+            while (n > 0) {
+                result -= 1;
+                n -= 1;
+            }
+        } else {
+            /*
+                When n is negative, we treat the subtraction as a sum
+             */
+            result = sum(m, -n);
+        }
+
+        return result;
+
+    }
+
+    @Override
+    public int multiply(int m, int n) {
+
+        /*
+         * Contract: multiply(m, n) must sum the value of m for
+         * n times, and return the result
+         */
+
+        int result = 0;
+        boolean opposite = n < 0;
+        if (n < 0) n = -n;
+
+
+        while (n > 0) {
+            result = sum(result, m);
+            n -= 1;
+        }
+
+        if (opposite) result = -result;
+
+        return result;
+    }
+
+    @Override
+    public int divide(int m, int n) {
+
+        /*
+         * Contract: divide(m, n) must subtract n from n
+         * until it goes to 0, and return the result
+         */
+
+        if (n == 0) throw new ArithmeticException("Division by zero");
+        int result = 0;
+        boolean opposite1 = n < 0, opposite2 = m < 0;
+        if (opposite1) n = -n;
+        if (opposite2) m = -m;
+
+        while (m > 0) {
+            m = subtract(m, n);
+            result += 1;
+        }
+
+        if (m != 0) result -= 1;
+        if (opposite1 ^ opposite2) result = -result;
+
+        return result;
+    }
+}
---
+++ homework0/src/calculator/CalculatorTestPrototype.java
@@ -0,0 +1,47 @@
+package calculator;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTestPrototype {
+
+    @Test
+    public void testSum() {
+        /*
+            Tests the correctness of the sum between two positive numbers.
+         */
+        CalculatorPrototype calculator = new CalculatorPrototype();
+        int sum = calculator.sum(3,3);
+        assertEquals(6, sum);
+    }
+
+    @Test
+    public void testDivisionEven() {
+        /*
+            Tests the correctness of the division without remainder.
+         */
+        CalculatorPrototype calculator = new CalculatorPrototype();
+        int sum = calculator.divide(6,3);
+        assertEquals(2, sum);
+    }
+
+    @Test
+    public void testDivisionOdd() {
+        /*
+            Tests the correctness of the division with remainder.
+         */
+        CalculatorPrototype calculator = new CalculatorPrototype();
+        int sum = calculator.divide(7,3);
+        assertEquals(2, sum);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivisionByZero() {
+        /*
+            Tests the correctness of the division by zero.
+         */
+        CalculatorPrototype calculator = new CalculatorPrototype();
+        int sum = calculator.divide(3,0);
+    }
+
+}
